1912951927cf4e77c7a85701aae023a3a15cbf50
Myles Best <myles+github@empty.best>
2022-10-11 02:36:35 -0700

    Add README & Example

---
+++ README.md
@@ -0,0 +1,6 @@
+# Prowl Queue
+
+Some common logic around Tokio mpsc with retries for
+prowl notifications.
+
+https://docs.rs/prowl-queue/0.1.1/prowl_queue/
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,20 @@
+//! ## Example
+//! ```
+//! use prowl_queue::{ProwlQueue, ProwlQueueSender};
+//!
+//! fn application(sender: &ProwlQueueSender) {
+//!     let notification = prowl::Notification::new(...)
+//!     sender.add(notification).expect("Failed to add notification");
+//! }
+//!
+//! #[tokio::main]
+//! async fn main() {
+//!     let (sender, reciever) = ProwlQueue::default().into_parts();
+//!     tokio::spawn(reciever.async_loop());
+//!     application(&sender);
+//! }
+//! ```
+
 mod options;
 mod queue;
 
